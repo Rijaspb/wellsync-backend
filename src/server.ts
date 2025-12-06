df6d0eb926d76d0fb2116bbecd870bfe596cb420
Rijas Panthayil Baby Suresh <rijaspb@gmail.com>
2025-12-06 23:20:20 +0000

    Initial TypeScript Express server setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import 'dotenv/config'
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome Rijas')
+})
+
+//server
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
